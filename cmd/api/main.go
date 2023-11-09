7d8a7b088cca1f9c8f4bccbd79f776ee4c431b31
Raju Ghorai <raju.ghorai@everest.engineering>
2023-11-09 11:11:39 +0530

      Setup: Dir structure

---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Hello World")
+	})
+	r.Run(":8080")
+}
