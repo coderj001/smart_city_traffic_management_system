5b04ff403752d050ffb1e8934be7023777697daf
Raju Ghorai <rajughorai41410@gmail.com>
2023-11-09 19:30:38 +0530

    😁 Added: gorm connetDB.go and loadEnv.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+
+	"github.com/coderj001/smart_city_traffic_management_system/internal/initilizers"
+	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	config, err := initilizers.LoadConfig()
+	if err != nil {
+		log.Fatal("🚀 Could not load environment variables", err)
+	}
+	initilizers.ConnectDB(&config)
+}
 
 func main() {
 	r := gin.Default()
